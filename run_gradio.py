415fe32028ae717f35cdef4cdf726f6e318b8f74
Chenliang Wang <seifer08ms@users.noreply.github.com>
2024-01-09 22:54:07 +0800

    Update run_gradio.py

--- run_gradio.py
+++ run_gradio.py
@@ -1,17 +1,7 @@
-# first: pip install pysqlite3-binary
-# then in settings.py:
-import os
-# these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # 导入必要的库
 import gradio as gr
 from langchain.vectorstores import Chroma
