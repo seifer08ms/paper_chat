7983fa9440ff7014e643d0f5159c4f69eca17c08
Chenliang Wang <seifer08ms@users.noreply.github.com>
2024-01-09 22:14:56 +0800

    Update run_gradio.py

--- run_gradio.py
+++ run_gradio.py
@@ -19,7 +19,7 @@ def init():
 def load_chain():
     # 加载问答链
     # 定义 Embeddings
-    embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformer")
 
     # 向量数据库持久化路径
     persist_directory = 'data_base/vector_db/chroma'
@@ -30,7 +30,7 @@ def load_chain():
         embedding_function=embeddings
     )
 
-    llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
+    llm = InternLM_LLM(model_path = "Shanghai_AI_Laboratory/internlm-chat-7b")
 
     template = """使用以下上下文来回答用户的问题。如果你不知道答案，就说你不知道。总是使用中文回答。
     问题: {question}
